a52807ca4b7e519d98fc3be0b834e959a2fea670
Claudio-DiGiovanni <digioclaudio97@gmail.com>
2022-11-28 15:16:26 +0100

    First commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
 import Vue from 'vue';
 import App from './App.vue';
+
+library.add(fas, far, fab);
 
 Vue.config.productionTip = false;
 
